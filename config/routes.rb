d53a6e3fff6fe49989ced1869fe6e7c9728baafd
Kyle Dorman <ked65@cornell.edu>
2014-01-22 10:12:50 -0500

    added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Stackstacker::Application.routes.draw do
 
   resources :boards
 
-  root 'boards#index'
+  root :to => "boards#show", :id => '1'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Board.create(name:"Example Board", description: "This is an example board for my trello clone project. I have focused on The drag an drop capabilities of trello for this project.")
+
+(1..3).to_a.each do |num|
+  Stack.create(name: "Stack " + num.to_s, position: num, board_id: 1)
+end
+
+(1..6).to_a.each do |num|
+  stack_num = (num + 1) / 2
+  Card.create(name: "Card " + num.to_s, description: "This is card " + num.to_s + " for stack " + stack_num.to_s,
+    position: num % 2 + 1, stack_id: stack_num)
+end
